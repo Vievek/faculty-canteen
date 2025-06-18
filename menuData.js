5db63c4d6ad089b8f9b21542524ad0fc28632cf7
vievegan <vieveganthaneswaran@gmail.com>
2025-06-18 19:08:33 +0530

    feat: Update menuData with sample image URLs for menu items

--- menuData.js
+++ menuData.js
@@ -3,70 +3,79 @@ const MENU_ITEMS = [
     id: "kottu",
     name: "Chicken Kottu",
     price: 550,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://img.freepik.com/premium-photo/chicken-kothu-parotta-curried-shredded-indian-flatbread-popular-south-indian-street-food-made-with-layered-bread-pieces-meat-egg-vegetables-selective-focus_726363-644.jpg?w=1060",
     description: "Hot griddled roti shredded with chicken, egg & veggies",
   },
   {
     id: "veg-roti",
     name: "Vegetable Roti",
     price: 120,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://img.freepik.com/premium-photo/wheat-roti-hd-8k-wallpaper-stock-photographic-image_1064748-494.jpg?w=2000",
     description: "Tri-folded roti stuffed with mildly spiced vegetables",
   },
   {
     id: "milk-tea",
     name: "Milk Tea (Hot)",
     price: 80,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://th.bing.com/th/id/OIP.bBrGdbNiMDvUMjj0NNrkxgHaKE?r=0&rs=1&pid=ImgDetMain&cb=idpwebpc2",
     description: "Ceylon black tea brewed strong with condensed milk",
   },
   {
     id: "ice-coffee",
     name: "Iced Coffee",
     price: 180,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image: "https://wallpaperaccess.com/full/2186603.jpg",
     description: "Chilled sweet coffee with a hint of cardamom",
   },
   {
     id: "fish-bun",
     name: "Fish Bun",
     price: 140,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://th.bing.com/th/id/R.e7e961e19b7982e11303bbaf992c6f80?rik=V366sqX4x28lLw&pid=ImgRaw&r=0",
     description: "Soft bun filled with spicy fish sambol",
   },
   {
     id: "string-hoppers",
     name: "String Hoppers (5 pcs)",
     price: 200,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://th.bing.com/th/id/OIP.ECHxp0ghppcxWlztGJklUAHaEq?r=0&rs=1&pid=ImgDetMain&cb=idpwebpc2",
     description: "Steamed rice-flour nests served with coconut sambol",
   },
   {
     id: "egg-hopper",
     name: "Egg Hopper",
     price: 110,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://c8.alamy.com/comp/GA485A/sri-lankan-egg-hopper-bittara-aappa-GA485A.jpg",
     description: "Crispy bowl-shaped appa with a runny egg center",
   },
   {
     id: "pol-roti",
     name: "Pol Roti & Lunu Miris",
     price: 130,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://th.bing.com/th/id/OIP.tg44KOoZ332nRAgU9h2IgQHaFH?r=0&rs=1&pid=ImgDetMain&cb=idpwebpc2",
     description: "Coconut flatbread with spicy onion–chilli relish",
   },
   {
     id: "cbp",
     name: "Chocolate Biscuit Pudding",
     price: 250,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://www.shutterstock.com/image-photo/home-made-chocolate-biscuit-pudding-600w-1901724046.jpg",
     description: "Layered Marie biscuits soaked in chocolate cream",
   },
   {
     id: "faluda",
     name: "Rose Faluda",
     price: 220,
-    image: "ADD SAMPLE IMAGE URL FROM WEBSITE",
+    image:
+      "https://th.bing.com/th/id/OIP.wmHA75FfEyXvh1rjAEsmggAAAA?r=0&rs=1&pid=ImgDetMain&cb=idpwebpc2",
     description: "Rose-flavoured milkshake with ice cream & basil seeds",
   },
 ];
