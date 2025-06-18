ba3352de2f0652a6af6522887718e41918f7abd1
vievegan <vieveganthaneswaran@gmail.com>
2025-06-18 17:54:50 +0530

    refactor: Comment out unused imports and reset favItems to an empty array

--- app/(tabs)/favourites.js
+++ app/(tabs)/favourites.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { FlatList, Text, View } from "react-native";
-import menuData from "../../menuData";
-import { useRouterContext } from "expo-router";
+// import menuData from "../../menuData";
+// import { useRouterContext } from "expo-router";
 
 export default function Favourites() {
-  const { favourites } = useRouterContext();
-  const favItems = menuData.filter((item) => favourites.includes(item.id));
+  // const { favourites } = useRouterContext();
+  // const favItems = menuData.filter((item) => favourites.includes(item.id));
+  const favItems = [];
   if (favItems.length === 0) {
     return <Text>No favourites yet</Text>;
   }
+
   return (
     <FlatList
       data={favItems}
